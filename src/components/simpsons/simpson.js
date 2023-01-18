22d4795c0dc37a28b3076ec182ff735c3e5fa13a
Alifoxy <nightfoxy666@gmail.com>
2023-01-18 22:24:23 +0300

    commit

--- src/components/simpsons/simpson.js
+++ src/components/simpsons/simpson.js
@@ -10,7 +10,7 @@ export const DescSimp = () => {
         <div>
            <div>
                <h3>name: Homer</h3>
-               <h3>surname: Simpson</h3>
+               <h3>surname: Simpsons</h3>
                <img src={url1} alt="Homer Simpson"/>
                <div><b>description: Homer Jay Simpson (born May 12, 1956 or 1969 or 1977 or 1983) is the main protagonist of The Simpsons series (or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson. Homer is overweight (said to be ~240 pounds), lazy, and often ignorant to the world around him. Although Homer has many flaws, he has shown to have great caring, love, and even bravery to those he cares about and, sometimes, even others he doesn't. He also serves as the main protagonist of the The Simpsons Movie. Homer Simpson is 39 years old.</b></div>
            </div>
