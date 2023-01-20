2c9614e084cdf31a6dea79d570d8b0b845eb02a7
Alifoxy <nightfoxy666@gmail.com>
2023-01-20 17:18:48 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     return (
         <div className="App">
             <div className="info-block">
-                {!!users.length && <User users={users} onSelectUser={setUserPosts}/>}
+                {!!users.length && <User users={users} onSelectUser={setUsers}/>}
             </div>
             <div className="divider"/>
             <div className="post-block">
