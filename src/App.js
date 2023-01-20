3d21cd62c60c85f5064cf7c6482b86982a703f82
Alifoxy <nightfoxy666@gmail.com>
2023-01-20 15:25:51 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 //import DescRMchar from "./components/Rick_&_Morty/character";
-import DescSimp from './components/simpsons/simpson';
+// import DescSimp from './components/simpsons/simpson';
+import React, { useEffect, useState } from "react";
+import { User } from "./componets/Users/User";
+import { UserDets } from "./componets/Users/UserDets";
+import { GetInfo } from "./components/Users/GetInfo";
 
-export default DescSimp;
+function App() {
+    const [users, setUsers] = useState([]);
+    const [userPosts, setUserPosts] = useState(null);
+
+    useEffect(() => {
+        GetInfo().then(resp => setUsers(resp));
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="info-block">
+                {!!users.length && <User users={users} onSelectUser={setUserPosts}/>}
+            </div>
+            <div className="divider"/>
+            <div className="post-block">
+                {!!userPosts && <UserDets onRemoveUserPosts={setUserPosts} userPosts={userPosts}/>}
+            </div>
+        </div>
+    );
+}
+
+export default App;
+// export default DescSimp;
 // export default DescRMchar;
---
+++ src/components/Users/UserDets.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { getUserPosts } from "../components/Users/GetInfo";
+
+export const UserPosts = ({userId, onRemoveUserPosts}) => {
+    const [user, setUserPosts] = useState(null);
+    useEffect(() => {
+        if (userId) {
+            getUserPosts(userId).then(resp => setUserPosts(resp))
+        }
+    }, [userId])
+    console.log(user);
+    if (!user) return null;
+    return (
+        <div>
+            UserPosts
+            <div>posts: {user.post}</div>
+            <button onClick={() => onRemoveUserPosts(null)}>Clean up user posts</button>
+        </div>
+    );
+};
