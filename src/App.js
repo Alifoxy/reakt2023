9feb13123377fcac6882581fa1bf04a9f80eecdf
Alifoxy <nightfoxy666@gmail.com>
2023-01-22 23:41:22 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@
 //import {App} from './components/homework1/simpsons/Simpsons';
 
 //rick & morty project run
-import {App} from './components/homework1/rick_&_morty/Characters';
+//import {App} from './components/homework1/rick_&_morty/Characters';
 //-------------------------------------------------------------
 
 
 //-------------------------Homework2---------------------------
 //users project run
-//import {App} from './components/homework2/users/RunApp';
+import {App} from './components/homework2/users/RunApp';
 
 //spacex project run
 //import {App} from './components/homework2/spacex/RunApp';
