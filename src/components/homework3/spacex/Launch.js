3ff4cfdbcd816d189dc8f8457be82f3f568cf5c9
Alifoxy <nightfoxy666@gmail.com>
2023-01-24 14:58:31 +0300

    commit

--- src/components/homework3/spacex/Launch.js
+++ src/components/homework3/spacex/Launch.js
@@ -1,13 +1,13 @@
 
-export const User = ({launch}) => {
-    const {mission_name, launch_year} = launch;
-    if (launch_year < 2020 || launch_year > 2020){
-        return (
-            <div className={'launch_block'}>
-                <div>mission_name: {mission_name}</div>
-                <div>launch_year: {launch_year}</div>
-            </div>
-        );
-    }
+export const User = ({user}) => {
+    const {id, name, email} = user;
+
+    return (
+        <div className={'user_block'}>
+            <div>id: {id}</div>
+            <div>name: {name}</div>
+            <div>email: {email}</div>
+        </div>
+    );
 };
 
