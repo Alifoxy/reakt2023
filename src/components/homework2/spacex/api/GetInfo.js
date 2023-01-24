930f6db6ed377ef738ca323dc1f2bda7aa661783
Alifoxy <nightfoxy666@gmail.com>
2023-01-24 13:37:50 +0300

    commit

--- src/components/homework2/spacex/api/GetInfo.js
+++
@@ -1,3 +0,0 @@
-export const getLaunchesData = () => {
-    return fetch('https://api.spacexdata.com/v3/launches/').then(resp => resp.json());
-}
--- src/components/homework2/users/api/GetInfo.js
+++
@@ -1,6 +0,0 @@
-export const getUsers = () => {
-    return fetch('https://jsonplaceholder.typicode.com/users/').then(resp => resp.json());
-}
-export const getUserPosts = (id) => {
-    return fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`).then(resp => resp.json());
-}
