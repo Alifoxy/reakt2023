f4c5c81b4a5b10249578d7b2e82a244def4134f2
Alifoxy <nightfoxy666@gmail.com>
2023-01-25 13:54:49 +0300

    commit

--- src/components/homework3/userform/Users.js
+++ src/components/homework3/userform/Users.js
@@ -7,7 +7,7 @@ export const Users = ({users}) => {
             {users.map(user => {
                 const {id, username} = user;
                 return (
-                    <div key={id}>
+                    <div key={user.id}>
                         <div>id:{id}</div>
                         <div>username:{username}</div>
                     </div>
