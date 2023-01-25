050401e3a60aed9a2dbcf3c18d14867b1a85cb74
Alifoxy <nightfoxy666@gmail.com>
2023-01-25 14:43:45 +0300

    commit

--- src/components/homework3/userform/Users.js
+++ src/components/homework3/userform/Users.js
@@ -8,7 +8,7 @@ export const Users = ({users}) => {
                 const {id, username} = user;
                 return (
                     <div key={id}>
-                        <div>id:{id + 1}</div>
+                        <div>id:{id}</div>
                         <div>username:{username}</div>
                     </div>
                 )
