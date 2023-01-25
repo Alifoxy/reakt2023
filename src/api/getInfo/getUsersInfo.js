c1929c799d5219b126a5614e18b898a509232c6d
Alifoxy <nightfoxy666@gmail.com>
2023-01-25 13:43:55 +0300

    commit

--- src/api/getInfo/getUsersInfo.js
+++ src/api/getInfo/getUsersInfo.js
@@ -1,7 +1,8 @@
-import { axiosRequest } from "../axios/axiosConfig";
-import { urls } from "./endpoints";
+import {axiosRequest} from "../../axios/axiosConfig";
+import {urls} from "../endpoints";
 
 export const getUsers = {
     getAll: () => axiosRequest.get(urls.users),
     createUser: (newUser) => axiosRequest.post(urls.users, newUser),
 }
+
